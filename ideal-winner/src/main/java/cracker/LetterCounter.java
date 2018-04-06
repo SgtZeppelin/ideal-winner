20e16f404393af53f6448615dd335ae44e12aa40
gersc <gersc@DESKTOP-4UE8J8C.fritz.box>
2018-04-06 21:47:54 +0200

    Klasse LetterCounter hinzugefuegt Interface hinzugefuegt Main-Methode hinzugefuegt

---
+++ ideal-winner/src/main/java/cracker/LetterCounter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cracker;
+
+/**
+ *
+ * @author gersc
+ */
+public interface LetterCounter {
+    public LetterNumberPair[] count( String text );
+}
---
+++ ideal-winner/src/main/java/cracker/LetterNumberPair.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cracker;
+
+/**
+ *
+ * @author gersc
+ */
+public class LetterNumberPair {
+    private char letter;
+    private int number;
+
+    public LetterNumberPair(char letter, int number) {
+        this.letter = letter;
+        this.number = number;
+    }
+
+    public char getLetter() {
+        return letter;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    
+}
